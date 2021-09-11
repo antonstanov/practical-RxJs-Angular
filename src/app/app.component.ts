36016bfc1fcffc6fc3b64b9c2861464a485b0938
AVStanovskiy <avstanovskiy@tsum.ru>
2021-09-11 17:09:34 +0400

    prettier start

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,17 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
+import { timer } from "rxjs";
+import { map, tap } from "rxjs/operators";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.scss"],
 })
 export class AppComponent {
-  title = 'practical-RxJs-Angular';
+  title = "practical-RxJs-Angular";
 
-  timer$ = timer(1000, 1000).pipe(map(data => data + 1), tap(console.log), tap(_ => {}))
+  timer$ = timer(1000, 1000).pipe(
+    map((data) => data + 1),
+    tap(console.log)
+  );
 }
